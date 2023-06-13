926c6af4e636d2b12bf8c9a213a83c092ad2cd47
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-14 00:11:37 +0530

    b2-insert

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import {MongoClient } from "mongodb";
 
-async function main(){
+async function insertRecord(){
  const uri = "mongodb://127.0.0.1:27017";
  const  client = new MongoClient(uri);
 
  const db=client.db("mydb");
  const message = db.collection("message");
 
-
- const res = await message.insertOne({ message:"node 1"});
+let jsonDocument ={message:"hello world", to:'dhanu' ,from:'cdac'}
+  await message.insertOne({jsonDocument });
   await client.close();
 
 console.log("record added");
 }
 
+async function main(){
+  await insertRecord();
+}
 main();
 
 
