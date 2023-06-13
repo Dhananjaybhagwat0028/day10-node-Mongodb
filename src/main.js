ceb3d311fed32bac58d9b1753eed4bec721f48b8
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-14 02:11:08 +0530

    b9-ex-mongo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
-// remotely executable
-// Network call
-// Network Address
-// Remote address
-// http://localhost:4000/
+import { MongoClient } from "mongodb";
 import express from "express";
 const app = express();
 
+async function main(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let message = req.query.message;
+  let inputDocument = { message: message };
+  await messageColl.insertOne(inputDocument);
+
+  await client.close();
+
+  res.send("record added");
+}
+
+async function findAllMessage(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let list = await messageColl.find().toArray();
+
+  await client.close();
+  res.send(list);
+}
+
+app.get("/main", main);
+app.get("/find", findAllMessage);
+
+// http://loaclhost:4000
 app.listen(4000);
