9b3437f96f6bc7a54d1248ade81ac716d0bc1d66
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-13 23:21:47 +0530

    b1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import {} from "node:fs";
-console.log("hello"); 
+import {MongoClient } from "mongodb";
+
+async function main(){
+ const uri = "mongodb://127.0.0.1:27017";
+ const  client = new MongoClient(uri);
+
+ const db=client.db("mydb");
+ const message = db.collection("message");
+
+
+ const res = await message.insertOne({ message:"node 1"});
+  await client.close();
+
+console.log("record added");
+}
+
+main();
+
 
 
 
