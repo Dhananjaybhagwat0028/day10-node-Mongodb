ead1f0672e39a28829b69412e5fd7e3356b9d670
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-16 15:34:12 +0530

    express hello world

--- src/main.js
+++ src/main.js
@@ -1,38 +1,13 @@
-import { MongoClient } from "mongodb";
 import express from "express";
 const app = express();
 
-async function main(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
-
-  let message = req.query.message;
-  let inputDocument = { message: message };
-  await messageColl.insertOne(inputDocument);
-
-  await client.close();
-
-  res.send("record added");
+function main(req, res) {
+  // return "some message";
+  res.send("some message...!");
 }
 
-async function findAllMessage(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
-
-  let list = await messageColl.find().toArray();
-
-  await client.close();
-  res.send(list);
-}
-
+// http://localhost:4000/main
 app.get("/main", main);
-app.get("/find", findAllMessage);
 
 // http://localhost:4000
 app.listen(4000);
