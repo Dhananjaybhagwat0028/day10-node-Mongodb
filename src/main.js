bd8e56b08d4b0057feb36aeeeee50d49cb9cc97e
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-16 15:40:28 +0530

    more exprees fun

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ function main(req, res) {
   res.send("some message...!");
 }
 
-// http://localhost:4000/main
+function first(req, res) {
+  // logic...
+  res.send("Success");
+}
+
+// http://loaclhost:4000/main
 app.get("/main", main);
 
-// http://localhost:4000
+// http://loaclhost:4000/first
+app.get("/first", first);
+
+// http://loaclhost:4000
 app.listen(4000);
