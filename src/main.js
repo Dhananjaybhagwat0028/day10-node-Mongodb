96d76e3dd0f1bd1508e79137d5c7e68e3e525413
dhananjay bhagwat <dhananjaybhagwat777@gmail.com>
2023-06-16 16:29:44 +0530

    query ex

--- src/main.js
+++ src/main.js
@@ -1,17 +1,38 @@
+import { MongoClient } from "mongodb";
 import express from "express";
 const app = express();
 
-function main(req, res) {
-  res.send("Hello");
+async function main(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let message = req.query.message ;
+  let inputDocument = { message: message };
+  await messageColl.insertOne(inputDocument);
+
+  await client.close();
+
+  res.send("record added");
+}
+
+async function findAllMessage(req, res) {
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
+
+  const db = client.db("mydb");
+  const messageColl = db.collection("message");
+
+  let list = await messageColl.find().toArray();
+
+  await client.close();
+  res.send(list);
 }
 
 app.get("/main", main);
+app.get("/find", findAllMessage);
 
-app.get("/first", (req, res) => res.send("Hello i am network call"));
-
-app.get("/second", (req, res) => {
-  res.send("Hello SEcond!!");
-});
-
-// http://loaclhost:4000
+// http://localhost:4000
 app.listen(4000);
